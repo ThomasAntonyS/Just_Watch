98d91ea1cea0717cb2f999f67261c8d249db57f3
Thomas Antony S <srtomsam@gmail.com>
2024-06-12 21:02:20 +0530

    minor modifications

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import {FaGithubSquare, FaLinkedin, FaTwitterSquare} from 'react-icons/fa'
 
 function Footer() {
     return (
         <div className='footer'>
-            <div className="footer-links">
-                <a href="https://twitter.com/a0x001"><FaTwitterSquare className="foot" /></a>
-                <a href="https://www.linkedin.com/in/allan-maina-ab78011aa/"><FaLinkedin className="foot" /></a>
-                <a href="https://github.com/Allan-1"><FaGithubSquare className="foot" /></a>
-            </div>
-            <h1> Copyright © {new Date().getFullYear()} <a href='https://allanmaina.vercel.app/'>Allan</a></h1>
+            
+            <h1> Copyright © {new Date().getFullYear()} Thomas Antony S</h1>
         </div>
     )
 }
